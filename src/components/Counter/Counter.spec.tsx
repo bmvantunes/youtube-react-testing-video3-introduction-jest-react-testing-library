6d53dc1fc92a0d729be622ac754266b5b3059d40
unknown <bmvantunes@outlook.com>
2021-07-23 22:24:12 +0100

    after the video

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -1,7 +1,46 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Counter } from './Counter';
+import user from '@testing-library/user-event';
 
 describe('Counter', () => {
+  describe('initialized with defaultCount=10 and description="WWW"', () => {
+    beforeEach(() => {
+      render(<Counter defaultCount={10} description="WWW" />);
+    });
+
+    it('renders "Current Count: 10"', () => {
+      expect(screen.getByText('Current Count: 10')).toBeInTheDocument();
+    });
+
+    it('renders title as "WWW"', () => {
+      expect(screen.getByText(/WWW/)).toBeInTheDocument();
+    });
+
+    describe('when the incrementor changes to 5 and "+" button is clicked', () => {
+      beforeEach(() => {
+        user.type(screen.getByLabelText(/Incrementor/), '{selectall}5');
+        user.click(screen.getByRole('button', { name: 'Add to Counter' }));
+      });
+
+      it('renders "Current Count: 15"', () => {
+        expect(screen.getByText('Current Count: 15')).toBeInTheDocument();
+      });
+    });
+
+    describe('when the incrementor changes to 25 and "-" button is clicked', () => {
+      beforeEach(() => {
+        user.type(screen.getByLabelText(/Incrementor/), '{selectall}25');
+        user.click(
+          screen.getByRole('button', { name: 'Subtract from Counter' })
+        );
+      });
+
+      it('renders "Current Count: -15"', () => {
+        expect(screen.getByText('Current Count: -15')).toBeInTheDocument();
+      });
+    });
+  });
+
   describe('initialized with defaultCount=0 and description="My Counter"', () => {
     beforeEach(() => {
       render(<Counter defaultCount={0} description="My Counter" />);
@@ -17,7 +56,7 @@ describe('Counter', () => {
 
     describe('when - is clicked', () => {
       beforeEach(() => {
-        fireEvent.click(
+        user.click(
           screen.getByRole('button', { name: 'Subtract from Counter' })
         );
       });
@@ -29,7 +68,7 @@ describe('Counter', () => {
 
     describe('when + is clicked', () => {
       beforeEach(() => {
-        fireEvent.click(screen.getByRole('button', { name: 'Add to Counter' }));
+        user.click(screen.getByRole('button', { name: 'Add to Counter' }));
       });
 
       it('renders "Current count: -1"', () => {
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -7,20 +7,34 @@ export interface CounterProps {
 
 export function Counter({ description, defaultCount }: CounterProps) {
   const [count, setCount] = useState(defaultCount);
+  const [incrementor, setIncrementor] = useState(1);
 
   return (
     <div>
       <h2>
         DESC: {description} - DC: {defaultCount}
       </h2>
+      <label>
+        Incrementor:
+        <input
+          value={incrementor}
+          onChange={(evt) => {
+            setIncrementor(parseInt(evt.target.value) || 0);
+          }}
+          type="number"
+        />
+      </label>
       <button
         aria-label="Subtract from Counter"
-        onClick={() => setCount(count - 1)}
+        onClick={() => setCount(count - incrementor)}
       >
         -
       </button>
       Current Count: {count}
-      <button aria-label="Add to Counter" onClick={() => setCount(count + 1)}>
+      <button
+        aria-label="Add to Counter"
+        onClick={() => setCount(count + incrementor)}
+      >
         +
       </button>
     </div>
